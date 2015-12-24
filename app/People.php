50aa603cf96785ad6cabdf5857facdc75d9e7c8b
Dulguun B <duugi.com@gmail.com>
2015-12-25 01:15:19 +0800

    People model

---
+++ app/People.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Database\Eloquent\Model;
+
+class People extends Model
+{
+
+    //protected $table = 'news';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = [
+    //    'name', 'email', 'password',
+    //];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    //protected $hidden = [
+    //    'password', 'remember_token',
+    //];
+}
