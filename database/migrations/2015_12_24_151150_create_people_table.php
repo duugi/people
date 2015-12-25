18898563278790800f318c1d68791ff2b919882e
Dulguun B <duugi.com@gmail.com>
2015-12-25 13:17:08 +0800

    First

---
+++ database/migrations/2015_12_24_151150_create_people_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 100);
+            $table->string('lastname', 100);
+            $table->string('birth_year', 4);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('people');
+    }
+}
---
+++ database/migrations/2015_12_24_162933_create_role_user_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRoleUserTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('role_user', function(Blueprint $table) {
+			$table->integer('role_id')->unsigned();
+			$table->bigInteger('user_id')->unsigned();
+			$table->bigInteger('createdby')->unsigned();
+			$table->timestamp('startdate');
+			$table->timestamp('enddate')->nullable();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('role_user');
+	}
+}
